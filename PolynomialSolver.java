72b5189eac0b6f516d008cf21d377ef4375641cd
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2022-03-24 00:31:24 +0200

    sc

--- PolynomialSolver.java
+++ PolynomialSolver.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PolynomialSolver {
     class node {
         private Object element; 
@@ -43,6 +45,6 @@ public class PolynomialSolver {
 
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
