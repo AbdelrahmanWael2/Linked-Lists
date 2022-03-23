e01897558e29bcdc48c66344e2469edda1da4916
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2022-03-24 01:32:17 +0200

    ddd

--- PolynomialSolver.java
+++ PolynomialSolver.java
@@ -34,35 +34,35 @@ public class PolynomialSolver {
     private node head;
     private int size;
     public static boolean error = false;
-    public PolynomialSolver() 
-    {
+    public PolynomialSolver() {
         head = null;
         size = 0;
     } 
-
-    public void setPolynomial(char poly, int[][] terms, int length)
-    {
-        PolynomialSolver list = new PolynomialSolver();
-        node pointer = new node();
-        
-        pointer = head;
-        for(int i = 0 ; i < len ; i++)
-        {
-          
-            
-        }
-
-        
+    public void setPolynomial(char poly, int[][] terms){
+        System.out.print("Commit 2");
        
     }
+    
 
-
-
-    public static void main(String[] args) {
+    public static void set(){
         Scanner sc = new Scanner(System.in);
         String operation = sc.nextLine();
         char poly = sc.next().charAt(0);
         String coof = sc.nextLine();
+    }
+    
+
+
+    public static void main(String[] args) {
+        set();
+        Scanner sc = new Scanner(System.in);
+        String operation2 = sc.nextLine();
+        if(operation2.equals("set")){
+            set();
+        }
+        else if(operation2.equals("print")){
+            
+        }
     
     }
 }
