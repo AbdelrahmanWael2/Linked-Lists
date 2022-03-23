9dce5897e3a8b6110f3f91a9de845d125bc94ee0
abdelrahman <abdelrahmanwael50@gmail.com>
2022-03-24 01:14:24 +0200

    2

--- PolynomialSolver.java
+++ PolynomialSolver.java
@@ -34,12 +34,25 @@ public class PolynomialSolver {
     private node head;
     private int size;
     public static boolean error = false;
-    public PolynomialSolver() {
+    public PolynomialSolver() 
+    {
         head = null;
         size = 0;
     } 
-    public void setPolynomial(char poly, int[][] terms){
-        System.out.print("Commit 2");
+
+    public void setPolynomial(char poly, int[][] terms, int length)
+    {
+        PolynomialSolver list = new PolynomialSolver();
+        node pointer = new node();
+        
+        pointer = head;
+        for(int i = 0 ; i < len ; i++)
+        {
+          
+            
+        }
+
+        
        
     }
 
