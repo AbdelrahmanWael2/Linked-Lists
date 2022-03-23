36764355428b96f8da8be863c717400b415c36dc
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2022-03-24 00:22:17 +0200

    pol

---
+++ PolynomialSolver.java
@@ -0,0 +1,48 @@
+public class PolynomialSolver {
+    class node {
+        private Object element; 
+        private node next;
+        
+        public node (){
+            this.element=null;
+            this.next=null;
+        }
+    
+        public node(Object element, node next) {
+            this.element = element;
+            this.next = next;
+        }
+        
+        public void setNext(node nextNode){
+            this.next = nextNode;
+        }
+        
+        public void setElement(Object newElement){
+            this.element = newElement;
+        }
+        
+        public Object getElement(){
+         return this.element;
+        } 
+        
+        public node getNext(){
+         return this.next;
+        } 
+    }
+    private node head;
+    private int size;
+    public static boolean error = false;
+    public PolynomialSolver() {
+        head = null;
+        size = 0;
+    } 
+    public void setPolynomial(char poly, int[][] terms){
+        
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
